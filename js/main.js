0f56e6538260c0cd4f3c2bb74390d8725806491a
Mariapia Gianguzzi <piagianguzzi@gmail.com>
2020-12-01 20:40:28 +0100

    js-simon

--- js/main.js
+++ js/main.js
@@ -5,23 +5,26 @@ Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati
 
 /* CREO UNA VARIABILE DI NUMERI CASUALI CHE IL PC DEVE GENERARE DEI NUMERI RANDOM */
 
-var numeri = [67, 43, 3, 78, 12]; 
+var numeri = []; 
+console.log(numeri);
 
- /* CON MATH RANDOM IL PC GENERA 5 NUMERI CASUALI */
-alert ("I numeri casuali sono..." + numeri);
+/* CON MATH RANDOM DENTRO UN CICLO WHILE IL PC GENERA 5 NUMERI CASUALI, USO INCLUDES PERCHé DETERMINA SE UN DETERMINATO ELEMENTO è INCLUSO NELLA ARREY */
+i = 0;
+while (numeri.length < 5 ) {
+  var numeroRandom = Math.floor(Math.random() * 100 ) + 1;
 
-/* ALL'INTERNO DELLA FUNZIONE CREO LE VARIAVILI DOVE L'UTENTE INSERIRà I NUMERI APPENA VISTI */
-var userNumber = setInterval(myFunction, 3000);
-var userNumber = prompt("inserisci un numero alla volta");
-var userNumber = prompt("inserisci un numero alla volta");
-var userNumber = prompt("inserisci un numero alla volta");
-var userNumber = prompt("inserisci un numero alla volta");
-var userNumber = prompt("inserisci un numero alla volta");
+  if (numeri.includes(numeroRandom)) {
+  } else {
+    numeri.push(numeroRandom);
+  }
 
-function myFunction() {
-    
-    
+  i++
 } 
+
+
+
+
+
 
 
 

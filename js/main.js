b4c8bd336ab370e13d3f2fbef56227b2887fa0b8
Mariapia Gianguzzi <piagianguzzi@gmail.com>
2020-12-02 16:45:51 +0100

    js-simon finito

--- js/main.js
+++ js/main.js
@@ -1,31 +1,44 @@
 /* Un alert espone 5 numeri casuali diversi.
-Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
 Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati. */
 
+/* CREO DUE VARIABILI VUOTE DI NUMERI CASUALI CHE IL PC DEVE GENERARE E L'UTENTE DEVE INSERIRE*/
 
-/* CREO UNA VARIABILE DI NUMERI CASUALI CHE IL PC DEVE GENERARE DEI NUMERI RANDOM */
-
-var numeri = []; 
-console.log(numeri);
+var numeriPc = []; 
+var userNumbers = [];
+var numeriIndovinati = [];
 
 /* CON MATH RANDOM DENTRO UN CICLO WHILE IL PC GENERA 5 NUMERI CASUALI, USO INCLUDES PERCHé DETERMINA SE UN DETERMINATO ELEMENTO è INCLUSO NELLA ARREY */
-i = 0;
-while (numeri.length < 5 ) {
-  var numeroRandom = Math.floor(Math.random() * 100 ) + 1;
+while (numeriPc.length < 5) {
+  var numeroRandom = Math.floor(Math.random() * 100) + 1;
+  if (!numeriPc.includes(numeroRandom)) {
+    /* SE IL NUMERO RANDOM NON è INCLUSO NEL NUMERO PC ALLORA PUSH */
+    numeriPc.push(numeroRandom);
+  }
+}
+//console.log(numeriPc);
 
-  if (numeri.includes(numeroRandom)) {
-  } else {
-    numeri.push(numeroRandom);
+/* UN ALERT DICE ALL'ULTENTE DI RICORDARE I NUMERI CHE HA GENERATO IL PC */
+alert("Ricorda questi numeri: " + numeriPc + " Tra 30 secondi scrivi i numeri");
+
+/* CON IL METODO SETTIMEOUT CHIEDO ALL'UTENTE DI INSERIRE IN NUMERI  */
+
+//Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+setTimeout(function () {
+  for (var i = 0; i < 5; i++) {
+    var numeroUtente = Number(prompt("Inserisci un numero"));
+    userNumbers.push(numeroUtente);
   }
 
-  i++
-} 
+  //console.log(userNumbers);
 
-
-
-
-
-
-
-
+  //Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+  for (let j = 0; j < 5; j++) {
+    if (userNumbers.includes(numeriPc[j])) {
+      numeriIndovinati.push(numeriPc[j]);
+    }
+  }
+  //console.log(numeriIndovinati);
+  alert("Hai ricordato " + numeriIndovinati.length + " numeri");
+  alert("Ti sei ricordato i seguenti numeri: " + numeriIndovinati);
+}, 3000);
 
